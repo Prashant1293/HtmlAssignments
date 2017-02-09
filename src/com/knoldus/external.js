ec5609645537317e9d5715e5e4adf6c434ff7e32
Prashant <12ps93r@gmail.com>
2017-02-09 19:49:44 +0530

    Second Commit 9-02-17

--- src/com/knoldus/external.js
+++ src/com/knoldus/external.js
@@ -1,7 +1,62 @@
-function timerDisp(){
-var date= new Date();
-var time=date.toLocaleTimeString();
-document.getElementById("timer").innerHtml=time;
+function validateForm() {
+      if( document.myForm.username.value == "" )
+              {
+                 alert( "Please provide your name!" );
+                 document.myForm.username.focus() ;
+                 return false;
+              }
+    if( document.myForm.age.value == "" )
+                  {
+                     alert( "Please provide your Age!" );
+                     document.myForm.age.focus() ;
+                     return false;
+                  }
 
+if( document.myForm.gender.value == "" )
+              {
+                 alert( "Please provide your Gender!" );
+                 document.myForm.gender.focus() ;
+                 return false;
+              }
+
+if( document.myForm.email.value == "" )
+              {
+                 alert( "Please provide your Email!" );
+                 document.myForm.email.focus() ;
+                 return false;
+              }
+
+if( document.myForm.address.value == "" )
+              {
+                 alert( "Please provide your Address!" );
+                 document.myForm.address.focus() ;
+                 return false;
+              }
+return(true)
 }
-setInterval(function(){timerDisp()},1000)
+
+
+
+    function LoginFun(){
+            if( document.loginForm.userlogin.value == "" || document.loginForm.userpassword.value == "" )
+                          {
+                             alert( "Please provide your Detail First!" );
+                            return false;
+                          }
+         else if(document.loginForm.userlogin.value == "prashant" && document.loginForm.userpassword.value =="prashant")
+                          {
+            alert("welcome user::" + document.loginForm.userlogin.value);
+            document.write("Rest Of the code would be Covered Later On.")
+            return true
+            }
+            else
+            {
+            alert("Sorry No Record Found!!!!");
+            return false;
+            }
+            }
+
+function calculateArray(){
+alert("checkbox Covered")
+}
+
